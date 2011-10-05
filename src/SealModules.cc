d42b7948331f94047151e9931c3d0921dd9d1dc4
cmsbuild <>
2011-10-05 11:49:02 +0000

    This commit was manufactured by cvs2git to create tag 'RecoHI-HiTracking- CMSSW_5_0_0_pre3'.
    Sprout from master 2011-10-05 11:49:00 UTC Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch> 'adapt to PV configuration changes'
    Delete:
        interface/HiAnalyticalTrackSelector.h
        src/HiAnalyticalTrackSelector.cc
        src/SealModules.cc

--- src/SealModules.cc
+++
@@ -1,7 +0,0 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "RecoHI/HiTracking/interface/HiAnalyticalTrackSelector.h"
-
-using reco::modules::HiAnalyticalTrackSelector;
-
-DEFINE_FWK_MODULE(HiAnalyticalTrackSelector); 
